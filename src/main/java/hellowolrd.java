ab85ea345a10d8502b8308ed4c5429f894de6bdd
gw33973 <gw33973@ly.com>
2018-04-25 15:25:19 +0800

    hello

---
+++ src/main/java/hellowolrd.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)hellowolrd.java Created by gw33973 on 2018/4/25   15:24
+ * <p>
+ * Copyrights (C) 2018保留所有权利
+ */
+
+/**
+ * (类型功能说明描述)
+ *
+ * <p>
+ * 修改历史:                                 <br>  
+ * 修改日期           修改人员       版本       修改内容<br>  
+ * -------------------------------------------------<br>  
+ * 2018/4/25 15:24   gw33973     1.0       初始化创建<br>
+ * </p> 
+ *
+ * @author gw33973
+ * @version 1.0
+ * @since JDK1.7
+ */
+public class hellowolrd {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
